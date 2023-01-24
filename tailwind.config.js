6c2a0e0b6cf25cc6b2c44e733f1a2145f8687150
onursungu <onurcansungu@hotmail.com>
2023-01-24 12:11:53 +0300

    gega starter files

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+ content: ["./public/**/*.{html,js}"],  //Tailwind css lere ait olan classları yazacam dosyaları adresliyo
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
